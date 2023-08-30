6ee3f5de5c9666ab9229f2504953bd2f0c13d682
Javier Leon <javierleon@MacBook-Air-de-Javier.local>
2023-08-29 21:37:25 -0500

    Se inicia el projecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'developmentcrm';
+  constructor(private modalService: NgbModal, private userService: UserService) {
+    let hello$ = userService.helloWorld();
+
+    hello$.subscribe(
+      (data: any) => console.log(data),
+      err => console.error(err)
+    );
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
 }
