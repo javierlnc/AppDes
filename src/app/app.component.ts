1f3004a6eb03eea9bc1d4d7df741f6c78578fefd
Javier <javierlnc@icloud.com>
2023-09-01 16:34:26 +0000

    Importnuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UserService } from './services/user.service';
-
+import { AuthenticationService } from './services/authentication.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,16 +9,12 @@ import { UserService } from './services/user.service';
 })
 export class AppComponent {
   title = 'developmentcrm';
-  constructor(private modalService: NgbModal, private userService: UserService) {
-    let hello$ = userService.helloWorld();
+  constructor(private authenticationService: AuthenticationService) {
+    let user$ = authenticationService.login("johnny", "kleptocracy");
 
-    hello$.subscribe(
-      (data: any) => console.log(data),
-      err => console.error(err)
-    );
-  }
-
-  public open(modal: any): void {
-    this.modalService.open(modal);
+        user$.subscribe(
+            (data: any) => console.log(data),
+            err => console.error(err)
+        );
   }
 }
