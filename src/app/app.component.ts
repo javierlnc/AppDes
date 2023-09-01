05b8c1c84b4ebe9c8d181a971fe7e0aea6f53cf8
Javier <javierlnc@icloud.com>
2023-09-01 19:35:04 +0000

    Se crean los modulos home y user y se enruta userboton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,9 @@
 import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { UserService } from './services/user.service';
-import { AuthenticationService } from './services/authentication.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'developmentcrm';
-  constructor(private authenticationService: AuthenticationService) {
-    let user$ = authenticationService.login("johnny", "kleptocracy");
-
-        user$.subscribe(
-            (data: any) => console.log(data),
-            err => console.error(err)
-        );
-  }
+  title = 'Tiquetes & Encomiendas'
 }
