2c46901622db3fc9ad4c7e9c0db45d7a87e1b581
Javier <javierlnc@icloud.com>
2023-09-11 20:08:04 +0000

    se verifican componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './user/authentication.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tiquetes & Encomiendas'
-}
+  title = 'Tiquetes & Encomiendas'; constructor(private authenticationService: AuthenticationService){
+  let user$ = authenticationService.login("johnny", "kleptocracy");
+
+        user$.subscribe(
+            (data: any) => console.log(data),
+            err => console.error(err)
+        );
+  }
+      }
+
