afd23df631e434fb42e52f24e20d11132713a277
Javier <javierlnc@icloud.com>
2023-09-14 21:24:13 +0000

    cambio en envios

--- src/app/componets/envios/envios.component.ts
+++ src/app/componets/envios/envios.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Form, FormBuilder, FormGroup } from '@angular/forms';
 @Component({
-  selector: 'app-envios',
+  selector: 'envios',
   templateUrl: './envios.component.html',
   styleUrls: ['./envios.component.css']
 })
 export class EnviosComponent implements OnInit {
+  remitente: FormGroup;
+  constructor(bulder : FormBuilder){
+    this.remitente = bulder.group({
+
+    });
+
+  }
   ngOnInit(): void {
   }
+  onSubmit(form: any) : void{
+    console.log("enviando el siguiente tiquete", form)
+
+  }
+
 
 }
