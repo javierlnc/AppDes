2c036e96cf8195a1cb06037e39768bc07b75933a
Javier <javierlnc@icloud.com>
2023-09-16 16:17:53 +0000

    correcion tiquetes form

--- src/app/componets/tiquetes/tiquetes.component.ts
+++ src/app/componets/tiquetes/tiquetes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup} from '@angular/forms';
+import { Viajero } from 'src/app/models/viajero';
 
 @Component({
   selector: 'app-tiquetes',
@@ -6,13 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tiquetes.component.css']
 })
 export class TiquetesComponent implements OnInit {
+  destinos : String[] = ["caracoli", "bello","Medellin","Cualquiera","cualquiera dos"];
+  tiquete : FormGroup;
+
+  constructor(private fb : FormBuilder){
+    
+    this.tiquete = this.fb.group({
+      cedula: '',
+      nombre : '',
+      celular : '',
+      origen : '',
+      destino: '',
+      vehiculo: '',
+      silla: '',
+      precio: '',
+  
+    });
+  }
+
   ngOnInit(): void {
   }
 
-  onSubmit(form: any) : void{
-    console.log("enviando el siguiente tiquete", form)
+  onSubmit(value: String) : void{
+    console.log("enviando el siguiente tiquete", value)
 
   }
-  destinos : String[] = ["caracoli", "bello","Medellin","Cualquiera","cualquiera dos"]
-
+  
 }
---
+++ src/app/models/viajero.ts
@@ -0,0 +1,10 @@
+export interface Viajero{
+    cedula: string;
+    nombre : string
+    celular : string;
+    origen : string;
+    destino: string;
+    vehiculo: string;
+	silla: string;
+    precio: string;
+}
