20ebf2fcde4bcc4832bee0686e18011520dffc3a
javierlnc <javierlnc@icloud.com>
2023-09-16 22:19:35 -0500

    modificacion en campo de user

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,9 +1,9 @@
 export interface User {
     id: string;
-    firstName : string
-    lastName : string;
-    phoneNumber : string;
+    firstName: string
+    lastName: string;
+    phoneNumber: string;
     email: string;
-    authorityNames: string[];
-	username: string;
+    role: string;
+    username: string;
 }
